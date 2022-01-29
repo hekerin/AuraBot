233652c2ca95c9dae3b85dd6efba07b90481681b
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-01-29 22:26:38 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,51 @@
+let fs = require('fs')
+global.owner = ['628112958665', '6288215569001','6281393227036', '6283175998566', '6285693602003', '6283153189868'] // Letakan nomor kamu disini
+global.APIs = { // API Prefix
+  // nama: 'https://website'
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://api.neoxr.eu.org/',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  bx: 'https://bx-hunter.herokuapp.com',
+
+}
+global.APIKeys = { // APIKey nya disini
+  // 'https://website': 'apikey'
+  'https://api.neoxr.eu.org/': 'yntkts',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://api.xteam.xyz': 'HIRO',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'FVe0JFCdME58YSKmhoDWdRtyuJt',
+  'https://api.lolhuman.xyz': '929b48b9aa5a91abed8526cd',
+  'https://api.vhtear.com': 'sayahafiz',
+  'http://zekais-api.herokuapp.com': 'grqgD6pU',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+}
+
+// Sticker WM
+global.packname = 'AuraBot'
+global.author = 'Rifai'
+
+global.wait = '「 ⏱️ 」Harap tunggu...'
+global.eror = '「❗」Server ERROR!'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.watermark = '© Haruno'
+global.image = 'https://telegra.ph/file/63560c3d19d6bcce228f5.jpg'
+global.thumbfoto = 'https://telegra.ph/file/39bbded9693c9338069fd.jpg'
+
+global.multiplier = 59 // Semakin tinggi, semakin sulit naik level
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
