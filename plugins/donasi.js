ddce792e218d1bb4e49cc90591bc7b269af2c9d4
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-01-29 21:32:42 +0800

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
+┌〔 Donasi • Emoney 〕
+├ GoPay: -
+├ OVO: -
+├ Dana: 6281393227036
+├ Pulsa (Telkomsel): 6281393227036
+└────
+`.trim(), watermark, 'Menu', '.menu', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
