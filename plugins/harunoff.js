5988847b99c313aba586d35ad8f763d15cb1e733
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-02-07 14:27:07 +0800

    Update cuy

--- plugins/harunoff.js
+++ plugins/harunoff.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
   try {
     let gc1 = '628112958665-1625393837@g.us'
-    let gc2 = '628112958665-1628163967@g.us'
+    let gc2 = '6281393227036-120363041039565084@g.us'
     let gc3 = '628112958665-1571053173@g.us'
     let _gc1 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc1)
     let _gc2 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc2)
@@ -24,7 +24,7 @@ Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
 `.trim()
     await conn.send2ButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), caption, watermark, 'Owner', '.owner', 'Sewa', '.sewa', m, { contextInfo: { mentionedJid: m.sender } })
   } catch (e) {
-    let res = await fetch('https://raw.githubusercontent.com/FadliDarmawan/haruno-server/main/files/src.json')
+    let res = await fetch('https://raw.githubusercontent.com/RFIunknown/AuraBot-server/a3aedeae99434ab20250172b2fb6c48bb2bd509f/files/src.json')
     let json = await res.json()
     let tulisan = `
 *Join juga group official AuraBot!*
