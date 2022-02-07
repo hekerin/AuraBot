6c08b1fb01c83c4ec4f499ade65f3545474310fa
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-02-07 09:25:12 +0800

    Mungkin fix :v

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,26 +1,23 @@
-let yts = require('yt-search')
-let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Harap masukkan query yang ingin dicari!\n\nContoh: ${usedPrefix + command} video protes tiananmen square 1989`
-  let results = await yts(text)
-  let teks = results.all.map(v => {
-    switch (v.type) {
-      case 'video': return `
-*${v.title}* (${v.url})
-Durasi: ${v.timestamp}
-Diupload ${v.ago}
-${v.views} Penonton
-      `.trim()
-      case 'channel': return `
-*${v.name}* (${v.url})
-_${v.subCountLabel} (${v.subCount}) Subscriber_
-${v.videoCount} video
-`.trim()
-    }
-  }).filter(v => v).join('\n========================\n')
-  m.reply(teks)
+let limit = 50
+const { servers, yta } = require('../lib/y2mate')
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+  if (!args || !args[0]) throw `Harap masukkan URL Youtube yang ingin di download!\n\nContoh: ${usedPrefix + command} https://youtu.be/zyJJlPSeEpo`
+  let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+*Judul:* ${title}
+*Ukuran File:* ${filesizeF}
+`.trim(), m, null, {
+    asDocument: chat.useDocument, mimetype: 'audio/mp4'
+  })
 }
-handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['internet']
-handler.command = /^yts(earch)?$/i
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
+handler.tags = ['downloader']
+handler.command = /^yt(a|mp3)|mp3$/i
+handler.limit = true
 
 module.exports = handler
