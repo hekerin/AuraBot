c91dabe05cd4e3d9a39103d6ebda83162c1439eb
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-02-23 20:06:16 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, usedPrefix, args, command }) => {
             "listMessage": {
               "title": `${ucapan()}, ${name}`.trim(),
               "description": "Berikut ini adalah daftar harga AuraBot.",
-              "footerText": "Silahkan tekan tombol \"Click Here\" untuk melihat daftar harga Haruno Bot.\n\nJika ada pertanyaan atau semacamnya silahkan hubungi owner.",
+              "footerText": "Silahkan tekan tombol \"Click Here\" untuk melihat daftar harga AuraBot.\n\nJika ada pertanyaan atau semacamnya silahkan hubungi owner.",
               "buttonText": "Click Here",
               "listType": "SINGLE_SELECT",
               "sections": [
