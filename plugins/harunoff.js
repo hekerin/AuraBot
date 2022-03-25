26b933d71a8f9c924d65ee696383fe4e374fac9c
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-03-25 19:00:19 +0800

    Update harunoff.js

--- plugins/harunoff.js
+++ plugins/harunoff.js
@@ -46,5 +46,5 @@ Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
 }
 handler.help = ['groupofficial']
 handler.tags = ['main']
-handler.command = /^harunoff|gr(u|ou)(p|b)official|gcofficial$/i
+handler.command = /^auragc|gr(u|ou)(p|b)official|gcofficial$/i
 module.exports = handler
