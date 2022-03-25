614d8e8afb2f70e41e30464e25d5924dd4a0d7e7
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-03-25 18:41:03 +0800

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn }) => {
  await conn.sendFile(m.chat, global.API('xteam', '/randomimage/ass', {}, 'APIKEY'), 'ass.png', 'Nih Foto hentai-nya mau coli ya hati-hati Entar ketahuan', m, false, {thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['hentai']
-handler.tags = ['anime']
+handler.tags = ['premium']
 handler.command = /^hentai$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
-handler.private = true
+handler.private = false
 
 handler.admin = false
 handler.botAdmin = false
