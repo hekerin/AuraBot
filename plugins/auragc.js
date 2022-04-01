7714ed4b25c5504ba93ce4207d627fe98a68c012
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-04-01 19:49:45 +0800

    Ubah grup

--- plugins/auragc.js
+++ plugins/auragc.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
   try {
-    let gc1 = '628112958665-1625393837@g.us'
+    let gc1 = '6281393227036-120363037529652885@g.us'
     let gc2 = '6281393227036-120363041039565084@g.us'
     let gc3 = '628112958665-1571053173@g.us'
     let _gc1 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc1)
@@ -10,12 +10,16 @@ let handler = async (m, { conn }) => {
     let caption = `
 *Join juga group official AuraBot!*
 Sekedar meramaikan, atau juga jika ada informasi mengenai bot.
+
 *Group 1*
 ${_gc1}
+
 *Group 2*
 ${_gc2}
+
 *Group 3*
 ${_gc3}
+
 Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
 `.trim()
     await conn.send2ButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), caption, watermark, 'Owner', '.owner', 'Sewa', '.sewa', m, { contextInfo: { mentionedJid: m.sender } })
