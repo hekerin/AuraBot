7c6e8be0fbee3e275ca68bac7110ba42f2948429
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-04-16 14:23:26 +0800

    Update 2.5.0

--- plugins/menu.js
+++ plugins/menu.js
@@ -347,7 +347,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
               "rows": [{
                 "title":  `Nsfw`,
                 "description": "Menu 18+",
-                "rowId": `${_p}? jadibot`
+                "rowId": `${_p}? nsfw`
               }],
               "title": "─────「 21 」"
             }, {
